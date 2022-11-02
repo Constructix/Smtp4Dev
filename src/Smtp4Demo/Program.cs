10ae5f308ac00156e4c13131984e9fee72766749
Richard Jones <rjjones79@hotmail.com>
2022-11-02 15:23:31 +1000

    Updated email addresses. Since using smtp4Dev, can put in dummy email addresses and should be sent to the smtp4dev local email server, without the need for real email addresses.

--- src/Smtp4Demo/Program.cs
+++ src/Smtp4Demo/Program.cs
@@ -4,8 +4,8 @@ using MimeKit;
 using MimeKit.Text;
 
 var email = new MimeMessage();
-email.From.Add(MailboxAddress.Parse(""));
-email.To.Add(MailboxAddress.Parse(""));
+email.From.Add(MailboxAddress.Parse("test@yahoo.com"));
+email.To.Add(MailboxAddress.Parse("firstChance@google.com"));
 email.Subject = "Testing sending email from console .NET Core";
 email.Body = new TextPart(TextFormat.Html) { Text = "<h1>This is a header<h1><p>This is more text to go here."};
 
