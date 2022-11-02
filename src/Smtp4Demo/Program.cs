406ad1cef028e91cf683b5bad0dc0947ae290c1d
Richard Jones <rjjones79@hotmail.com>
2022-11-02 15:18:47 +1000

    Initial Commit

---
+++ src/Smtp4Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+var email = new MimeMessage();
+email.From.Add(MailboxAddress.Parse(""));
+email.To.Add(MailboxAddress.Parse(""));
+email.Subject = "Testing sending email from console .NET Core";
+email.Body = new TextPart(TextFormat.Html) { Text = "<h1>This is a header<h1><p>This is more text to go here."};
+
+using var smtp = new SmtpClient();
+smtp.Connect("localhost", 2525, false);
+
+smtp.Send(email);
+smtp.Disconnect(true);
